cb8df0e49f556b9cafba1e6df5b7b44c76110de7
Otto <fa@ijust.cc>
2023-10-08 17:21:50 +0800

    fix: router bug fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,30 +9,38 @@ const router = createRouter({
 })
 
 // router before
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, from) => {
   const token = localStorage.getItem('access_token')
   const expire = localStorage.getItem('expired_at')
   // debugger
 
+  const isUnLogin = !token && to.path !== '/login'
   // 未登录退出
-  if (!token && to.path !== '/login') {
-    next('/login')
+  if (isUnLogin) {
+    return {
+      path: '/login'
+    }
   }
   // 登录过期重新登录
   if (expire && new Date().getTime() > Number(expire) * 1000) {
     Message.error('登录过期，请重新登录')
-    next('/login')
+    localStorage.removeItem('access_token')
+    localStorage.removeItem('expired_at')
+    return {
+      path: '/login'
+    }
   }
 
   // 已登录的，不允许进入 /login 页面自动到 /
   if (token && to.path === '/login') {
-    next('/')
+    return {
+      path: '/'
+    }
   }
 
   if (to.meta.title) {
     document.title = to.meta.title as string
   }
-  next()
 })
 
 export default router
